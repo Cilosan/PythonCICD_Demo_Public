7d98e43f3d67d0d5c1cddaced1ebead55679eea5
Cilosan <126508131+Cilosan@users.noreply.github.com>
2023-11-21 07:31:27 +0100

    test + app
    test datei und app hinzugefügt

---
+++ app.py
@@ -0,0 +1,8 @@
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = float(input("Gib die erste Zahl ein: "))
+    num2 = float(input("Gib die zweite Zahl ein: "))
+    result = add_numbers(num1, num2)
+    print(f"Das Ergebnis ist: {result}")
