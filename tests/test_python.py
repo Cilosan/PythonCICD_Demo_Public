9d07eea08480abfa8235f9a9e8752c17f46656b1
Cilosan <126508131+Cilosan@users.noreply.github.com>
2023-11-28 07:37:30 +0100

    Update test_python.py

--- tests/test_python.py
+++ tests/test_python.py
@@ -1,7 +1,7 @@
-import python_app
+from python_app import add_numbers
 
 def test_addition():
-    assert python_app.add_numbers(3, 1) == 4
-    assert python_app.add_numbers(8, -2) == 6
-    assert python_app.add_numbers(0, -2) == -2
+    assert add_numbers(3, 1) == 4
+    assert add_numbers(8, -2) == 6
+    assert add_numbers(0, -2) == -2
 
